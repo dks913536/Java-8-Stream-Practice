f5fa5ba605f2bd81965654ff3b280b4f69ddf2cd
Deepak Kumar Singh <dks913536@gmail.com>
2026-09-21 12:28:06 +0530

    Add P05 - Convert strings to lowercase

---
+++ src/main/java/com/java8/set01_basics/P05_ToLowercaseStrings.java
@@ -0,0 +1,24 @@
+package com.java8.set01_basics;
+
+/**
+ *  Problem 04: Convert all strings to uppercase.
+ *  Input:  ["java", "spring", "boot", "microservices"]
+ *  Output: * ["JAVA", "SPRING", "BOOT", "MICROSERVICES"] */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class P05_ToLowercaseStrings {
+    public static void main(String[] args) {
+        List<String> str= Arrays.asList("java", "spring", "boot", "microservices");
+
+        List<String> upperCase=str.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+        System.out.println(upperCase);
+    }
+}
+
+
+//Stream used: map() — transforms each string into uppercase.
