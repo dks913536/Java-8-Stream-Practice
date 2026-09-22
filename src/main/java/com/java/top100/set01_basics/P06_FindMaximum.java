38b01e45b66e0a854bf7ce9f3ff4eaa2b8c81941
Deepak Kumar Singh <dks913536@gmail.com>
2026-09-22 07:49:33 +0530

    Add P06 - Find maximum number using Stream API

--- src/main/java/com/java/top100/set01_basics/P06_FindMaximum.java
+++ src/main/java/com/java/top100/set01_basics/P06_FindMaximum.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Problem 05: Remove duplicate elements from a list.
+ * Problem 06: Find the maximum number from a list.
  * Input: * [10, 20, 10, 30, 20, 40, 30]
  * Output: 40 */
 public class P06_FindMaximum {
